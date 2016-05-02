b40a2e82e2b8eb45395fe4c6746d3a252bb57d77
CAPSLOCKUSER <m.miklos05@gmail.com>
2016-05-02 23:22:38 +0200

    request-promise

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('colors');
 
 const getVideoFiles = require('./lib/get-video-files');
 const getHash = require('./lib/get-hash');
+const requestSubDB = require('./lib/request-subdb.js');
 
 program
   .version('1.0.0')
@@ -15,23 +16,18 @@ program
 
 console.log('language: ', program.language);
 
-/*fs.readdirSync('./')
-  .filter(isVideo)
-  .forEach(videoFile => {
-    getHash('./' + videoFile)
-      .then(hash => {
-        console.log(videoFile.green);
-        console.log(hash.red);
-      })
-      .then();
-  });*/
-
 getVideoFiles('./')
   .then(getHash)
-  .then(hashes => {
-    hashes.forEach(hash => {
-      console.log(hash.green);
-    });
-  });
-  //.then(requestSubDB)
+  //.then(hashes => {
+  //  hashes.forEach(hash => {
+  //    console.log(hash.green);
+  //  });
+  //});
+  .then(hashes => requestSubDB(hashes, program.language))
+  .then(response => {
+    console.log(response.statusCode);
+    //response.forEach(res => {
+    //  console.log('Response:', res)
+    //});
+  })
   //.then(saveSubtitles);
